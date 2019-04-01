7a30a861645450aee02eccfdea33cca66636bb4e
Taylor Wilcox <tjwilcox@ksu.edu>
2019-04-01 11:55:57 -0500

    Lab 25 Complete.

--- Ksu.Cis300.PriorityQueueLibrary/LeftistTree.cs
+++ Ksu.Cis300.PriorityQueueLibrary/LeftistTree.cs
@@ -13,8 +13,13 @@ namespace Ksu.Cis300.PriorityQueueLibrary
     /// An immutable generic binary tree node that can draw itself.
     /// </summary>
     /// <typeparam name="T">The type of the elements stored in the tree.</typeparam>
-    public partial class BinaryTreeNode<T> : ITree
+    public partial class LeftistTree<T> : ITree
     {
+        /// <summary>
+        /// Stores the null path length of the tree.
+        /// </summary>
+        private int _nullPathLength;
+
         /// <summary>
         /// Gets the data stored in this node.
         /// </summary>
@@ -23,24 +28,42 @@ namespace Ksu.Cis300.PriorityQueueLibrary
         /// <summary>
         /// Gets this node's left child.
         /// </summary>
-        public BinaryTreeNode<T> LeftChild { get; }
+        public LeftistTree<T> LeftChild { get; }
 
         /// <summary>
         /// Gets this node's right child.
         /// </summary>
-        public BinaryTreeNode<T> RightChild { get; }
+        public LeftistTree<T> RightChild { get; }
 
         /// <summary>
-        /// Constructs a BinaryTreeNode with the given data, left child, and right child.
+        /// Constructs a LeftistTree with the given data, left child, and right child.
         /// </summary>
         /// <param name="data">The data stored in the node.</param>
         /// <param name="left">The left child.</param>
         /// <param name="right">The right child.</param>
-        public BinaryTreeNode(T data, BinaryTreeNode<T> left, BinaryTreeNode<T> right)
+        public LeftistTree(T data, LeftistTree<T> left, LeftistTree<T> right)
         {
             Data = data;
-            LeftChild = left;
-            RightChild = right;
+            if (NullPathLength(right) < NullPathLength(left))
+            {
+                RightChild = right;
+                LeftChild = left;
+            }
+            else
+            {
+                RightChild = left;
+                LeftChild = right;
+            }
+            _nullPathLength = NullPathLength(RightChild) + 1;
+        }
+
+        public static int NullPathLength(LeftistTree<T> t)
+        {
+            if (t == null)
+            {
+                return 0;
+            }
+            return t._nullPathLength;
         }
     }
 }
